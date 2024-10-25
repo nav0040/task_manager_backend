8e79190e3ea26ec74969d3792ca4c30a541d5740
ImNaveen <naveendev004@gmail.com>
2024-10-25 16:36:16 +0530

    issues solved

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 
 exports.authenticateUser  = async (req, res, next) => {
   const token = req.cookies.token;
-  // console.log(req.cookies.token);
+  console.log(req.cookies);
 
   if (!token) return res.status(401).json({ success: false, message: "Unauthorized - no token provided" });
 
