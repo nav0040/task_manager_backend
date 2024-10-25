4d76f03294af1f8e7558128bbf6149856b430890
ImNaveen <naveendev004@gmail.com>
2024-10-25 16:11:44 +0530

    issues solved

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -8,8 +8,6 @@ exports.generateTokenAndSetCookie = (res,userId)=>{
 
     res.cookie('token',token,{
         httpOnly:true,
-        secure:process.env.NODE_ENV === 'production',
-        sameSite:'strict',
         maxAge:7 * 24 * 60 * 60 * 1000,
     });
 
